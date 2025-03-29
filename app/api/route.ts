3682603f2ae20f0bfa074cab027390c3239db853
Suryansh <suryanshvaish1@gmail.com>
2025-03-29 22:08:05 +0530

    Updated files

--- app/api/route.ts
+++ app/api/route.ts
@@ -2,8 +2,7 @@ import { log } from "console";
 import { NextRequest } from "next/server";
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  console.log(body);
+  await req.json();
 
   return Response.json({
     msg: "Account Created",
